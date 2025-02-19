5518903fae4f07e25cccabcf3aa75af710828517
isabellasz09 <isabellacarolinadesouza17@gmail.com>
2025-02-19 19:18:02 -0300

    Create tuplas_.py

---
+++ tuplas_.py
@@ -0,0 +1,7 @@
+vendas = ("Isabella",'19/02/25','17/07/09',2000,'Estagiaria')
+nome,data_cont,data_nasc,salario,cargo = vendas
+print(nome)
+print(data_cont)
+print(data_nasc)
+#print(salario)
+print(cargo)
