32e6be7b9a1a3c075ad078e0397afcd2f35a85f1
isabellasz09 <isabellacarolinadesouza17@gmail.com>
2025-02-19 19:12:17 -0300

    Create laco_while.py

---
+++ laco_while.py
@@ -0,0 +1,6 @@
+venda = input('Registre um produto. Para cancelar o registro de um novo produto, basta apertar enter sem digitar nada: ')
+vendas= []
+while venda != '':
+    vendas.append(venda)
+    venda = input('Registre um produto. Para cancelar o registro de um novo produto, basta apertar enter sem digitar nada: ')
+    print('Registros finalizados. As vendas cadastradas foram: {}'.format(vendas))
